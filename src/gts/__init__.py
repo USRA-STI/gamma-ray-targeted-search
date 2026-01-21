5e48520aa6b7dd54d32334dc0636ae68a1185656
Josh Wood <joshua.randall.wood@gmail.com>
2026-01-21 14:12:36 -0600

    add init files

---
+++ src/gts/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2017-2025 by Universities Space Research Association (USRA). All rights reserved.
+#
+# Developed by: William Cleveland, Adam Goldstein, and Alex Goberna
+#               Universities Space Research Association
+#               Science and Technology Institute
+#               https://sti.usra.edu
+#
+# Developed by: Daniel Kocevski and Joshua Wood
+#               National Aeronautics and Space Administration (NASA)
+#               Marshall Space Flight Center
+#               Astrophysics Branch (ST-12)
+#
+# Developed by: Lorenzo Scotton
+#               University of Alabama in Huntsville
+#               Center for Space Plasma and Aeronomic Research
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing permissions and limitations under the
+# License.
+#
+__version__ = "2.0.0"
