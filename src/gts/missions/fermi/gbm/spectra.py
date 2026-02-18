05723b4422c7f68ec25bac04985e18abce354e92
Josh Wood <joshua.randall.wood@gmail.com>
2026-02-18 15:55:37 -0600

    use gdt core functions for spectra definitions

--- src/gts/missions/fermi/gbm/spectra.py
+++ src/gts/missions/fermi/gbm/spectra.py
@@ -34,67 +34,40 @@
 #
 import numpy as np
 
-def soft():
-    """ Soft Spectral Template describing lower 1/3rd of GBM GRBs
+from gdt.core.spectra.functions import Band, Comptonized, BlackBody
 
-    Returns:
-        (func, dict): functional shape and dictionary containing function parameter values
-    """
-    return (band, {'epeak': 70.0, 'alpha': -1.9, 'beta': -3.70})
+# Soft Spectral Template describing lower 1/3rd of GBM GRBs
+soft = Band()
+soft.default_values = [
+      1.0, # Amplitude
+     70.0, # Epeak [keV]
+     -1.9, # alpha
+     -3.7, # beta
+    100.0, # Epiv [keV]
+]
 
-def norm():
-    """ Normal Spectral Template describing middle 1/3rd of GBM GRBs
+# Normal Spectral Template describing middle 1/3rd of GBM GRBs
+norm = Band()
+norm.default_values = [
+      1.0, # Amplitude
+    230.0, # Epeak [keV]
+     -1.0, # alpha
+     -2.3, # beta
+    100.0, # Epiv [keV]
+]
 
-    Returns:
-        (func, dict): functional shape and dictionary containing function parameter values
-    """
-    return (band, {'epeak': 230.0, 'alpha': -1.0, 'beta': -2.30})
+# Hard Spectral Template describing upper 1/3rd of GBM GRBs
+hard = Comptonized()
+hard.default_values = [
+       1.0, # Amplitude
+    1500.0, # Epeak [keV]
+      -0.5, # index
+     100.0, # Epiv [keV]
+]
 
-def hard():
-    """ Hard Spectral Template describing upper 1/3rd of GBM GRBs
-
-    Returns:
-        (func, dict): functional shape and dictionary containing function parameter values
-    """
-    return (comp, {'epeak': 1500.0, 'index': -0.5})
-            
-def band(params, energies):
-    """Band GRB function
-    This is evaluated in log space and then exponentiated at the end to
-    increase robustness.
-    
-    Args:
-        params (dict):
-            Dictionary containing band function parameters
-        energies (np.ndarray):
-            The energies at which to evaluate the function
-    
-    Returns:
-        (np.array): The evaluated function
-    """
-    e0 = params['epeak']/(2.0+params['alpha'])
-    ebreak = (params['alpha']-params['beta'])*e0
-    idx = (energies < ebreak)
-    logfxn = np.zeros(len(energies), dtype=float)
-    logfxn[idx] = np.log(params['amp']) + params['alpha']*np.log(energies[idx]/100.0) \
-                  - energies[idx]/e0
-    dindex = params['alpha']-params['beta']
-    idx = ~idx
-    logfxn[idx] = np.log(params['amp']) + dindex*np.log(dindex*e0/100.0) - \
-                dindex + params['beta']*np.log(energies[idx]/100.0)
-    return np.exp(logfxn)
-
-def comp(params, energies):
-    """Comptonized GRB function (Exponentially cut-off power law)
-    
-    Args:
-        params (dict):
-            Dictionary containing comptonized function parameters
-        energies (np.array):
-            The energies at which to evaluate the function
-    
-    Returns:
-        (np.ndarray): The evaluated function
-    """
-    return params['amp']*(energies/100.0)**params['index'] * \
-           np.exp(-energies*(2.0+params['index'])/params['epeak'])
+# Blackbody spectral template
+blackbody = BlackBody()
+blackbody.default_values = [
+     1.0, # Amplitude
+    10.0, # Temperature (kT) [keV]
+]
